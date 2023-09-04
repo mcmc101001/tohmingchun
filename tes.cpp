d66d62b6adce506e1d402d4efba391b938581a27
mcmc101001 <tohmingchun101001@gmail.com>
2023-09-04 12:26:36 +0800

    Add code block profile to personal website

---
+++ tes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class MingChun
+{
+private:
+  char *name;
+  int age;
+  char *name;
+
+public:
+  MingChun()
+  {
+    name = "Toh Ming Chun";
+    age = 22;
+  }
+  void education()
+  {
+    cout << "I am currently studying in NUS" << endl;
+  }
+};
